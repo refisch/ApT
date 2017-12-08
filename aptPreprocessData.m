520c25a312a18e9f37639ffbb57a8b6aec93abfe
Lukas Refisch <lukas.refisch@posteo.de>
2017-12-08 18:09:22 +0100

    Automatic detect whether use log10 of data or not. Also adjust printing.

--- aptPreprocessData.m
+++ aptPreprocessData.m
@@ -1,4 +1,4 @@
-function [sequence,Y,weightsY] = aptPreprocessData(sequence,Y,doLog10)
+function [sequence,Y,weightsY,doLog10] = aptPreprocessData(sequence,Y)
 %APTPREPROCESSDATA Does the preprocessing of the sequence data.
 %   Often multiple measurements to one sequence are performed. Her mean and
 %   weights calculation for regression model.
@@ -6,6 +6,18 @@ function [sequence,Y,weightsY] = aptPreprocessData(sequence,Y,doLog10)
 [uniSequence,~,idxSeq] = unique(sequence);
 yPP = zeros(1,length(uniSequence));
 stdyPP = yPP;
+
+for iGroups = 1:length(uniSequence)
+    yPP(iGroups) = mean(Y(idxSeq==iGroups));
+    stdyPP(iGroups) = std(Y(idxSeq==iGroups));
+end
+
+doLog10 = false;
+[~,pVal] = corr(yPP',stdyPP');
+if pVal < 0.01
+    doLog10 = true;
+end
+
 if doLog10
     if min(Y)<0
         Y = Y+abs(min(Y))+1;
@@ -14,10 +26,10 @@ if doLog10
         yPP(iGroups) = mean(log10(Y(idxSeq==iGroups)));
         stdyPP(iGroups) = std(log10(Y(idxSeq==iGroups)));
     end
-else
-    for iGroups = 1:length(uniSequence)
-        yPP(iGroups) = mean(Y(idxSeq==iGroups));
-        stdyPP(iGroups) = std(Y(idxSeq==iGroups));
+    [Rho,pVal] = corr(yPP',stdyPP');
+    if pVal<0.01
+        warning('Parson-Test suggests that errors are not log-normal distributed!:')
+        fprintf('Suggested correlation between mean (log10) and variance (log10) is %f with pvalue: %f',Rho,pVal)
     end
 end
 
--- aptWriteResults.m
+++ aptWriteResults.m
@@ -6,15 +6,22 @@ if(~exist('filename','var') || isempty(filename))
     filename = 'Results';
 end
 
+if stats.doLog10
+    digits = '5';
+else
+    digits = '1';
+end
+
 [beta_sorted,idxA] = sort(stats.beta(:,stats.Index1SE),'descend');
 predNames_sorted = predNames(idxA);
 idxPred = find(beta_sorted);
 fid = fopen([filename '.txt'],'w');
 fprintf(fid,'RootMeanSquaredError = %s\n\n', sqrt(stats.MSE(stats.Index1SE)));
-fprintf(fid,'Intercept:\t\t\t\t%.1f\n',stats.Intercept(stats.Index1SE));
+fprintf(fid,['Intercept:\t\t\t\t%.' digits 'f\n'],stats.Intercept(stats.Index1SE));
 fprintf(fid,'The %d contributing predictors, sorted with respect to effect size: \n \t\tPredictor\t Value\n',length(idxPred));
+
 for i = 1:length(idxPred)
-    fprintf(fid,'%23s\t\t\t%.1f\n ',predNames_sorted{idxPred(i)},beta_sorted(idxPred(i)));
+    fprintf(fid,['%23s\t\t\t%.' digits 'f\n'],predNames_sorted{idxPred(i)},beta_sorted(idxPred(i)));
 end
 
 fprintf(fid,'\n\n All Predictors were:\n');
