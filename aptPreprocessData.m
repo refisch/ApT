f45f4ae42f708b1d88f1af055ae4bdd86543da34
Lukas Refisch <lukas.refisch@posteo.de>
2017-12-08 16:50:46 +0100

    choose between log or non-log analysis

--- aptPreprocessData.m
+++ aptPreprocessData.m
@@ -1,4 +1,4 @@
-function [sequence,Y,weightsY] = aptPreprocessData(sequence,Y)
+function [sequence,Y,weightsY] = aptPreprocessData(sequence,Y,doLog10)
 %APTPREPROCESSDATA Does the preprocessing of the sequence data.
 %   Often multiple measurements to one sequence are performed. Her mean and
 %   weights calculation for regression model.
@@ -6,9 +6,19 @@ function [sequence,Y,weightsY] = aptPreprocessData(sequence,Y)
 [uniSequence,~,idxSeq] = unique(sequence);
 yPP = zeros(1,length(uniSequence));
 stdyPP = yPP;
-for iGroups = 1:length(uniSequence)
-    yPP(iGroups) = mean(Y(idxSeq==iGroups));
-    stdyPP(iGroups) = std(Y(idxSeq==iGroups));
+if doLog10
+    if min(Y)<0
+        Y = Y+abs(min(Y))+1;
+    end
+    for iGroups = 1:length(uniSequence)
+        yPP(iGroups) = mean(log10(Y(idxSeq==iGroups)));
+        stdyPP(iGroups) = std(log10(Y(idxSeq==iGroups)));
+    end
+else
+    for iGroups = 1:length(uniSequence)
+        yPP(iGroups) = mean(Y(idxSeq==iGroups));
+        stdyPP(iGroups) = std(Y(idxSeq==iGroups));
+    end
 end
 
 sequence = uniSequence;
