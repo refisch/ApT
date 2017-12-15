fa0cf529c9f03b37e176a2a2114a3807a69460da
Lukas Refisch <lukas.refisch@posteo.de>
2017-12-15 12:40:27 +0100

    rename plotting function

--- aptCompareFitVsTrue.m
+++ aptCompareFitVsTrue.m
@@ -1,28 +1,36 @@
-function aptCompareFitVsTrue(sequence, stats, Y)
+function kfit = aptCompareFitVsTrue(sequence, stats,doPlot)
 %COMPAREFITVSTRUE Summary of this function goes here
 %   Detailed explanation goes here
 
-ktrue = Y;
+
+ktrue = stats.Y;
 predX = aptPredictors(sequence);
-
 kfit = stats.Intercept(stats.Index1SE)+predX'*stats.beta(:,stats.Index1SE);
-
-figure
-s1 = subplot(2,1,1);
-scatter(ktrue,kfit,'r');
-hold on
-plot(min(ktrue):max(ktrue),min(ktrue):max(ktrue),'r--');
-hold off 
-
-s2 = subplot(2,1,2);
-scatter(10.^ktrue,10.^kfit,'b');
-hold on
-plot(min(10.^ktrue):max(10.^ktrue),min(10.^ktrue):max(10.^ktrue),'b--');
-hold off
-title(s1, 'on log10 scale')
-title(s2, 'backtransformed to lin scale')
-xlabel(s1,'true')
-ylabel(s1,'fitted')
-xlabel(s2,'true')
-ylabel(s2,'fitted')
+if doPlot
+    if stats.doLog10
+        figure
+        s1 = subplot(2,1,1);
+        scatter(ktrue,kfit,'r');
+        hold on
+        plot(min(ktrue):max(ktrue),min(ktrue):max(ktrue),'r--');
+        hold off
+        
+        s2 = subplot(2,1,2);
+        scatter(10.^ktrue,10.^kfit,'b');
+        hold on
+        plot(min(10.^ktrue):max(10.^ktrue),min(10.^ktrue):max(10.^ktrue),'b--');
+        hold off
+        title(s1, 'on log10 scale')
+        title(s2, 'backtransformed to lin scale')
+        xlabel(s1,'true')
+        ylabel(s1,'fitted')
+        xlabel(s2,'true')
+        ylabel(s2,'fitted')
+    else
+        figure
+        scatter(ktrue,kfit,'b');
+        hold on
+        plot(min(ktrue):max(ktrue),min(ktrue):max(ktrue),'b--');
+    end
+end
 end
--- aptPlotDataHists.m
+++ aptPlotDataHists.m
@@ -1,33 +1,24 @@
-function aptPlotSummaryResults(stats,Y,sequence,weightsY)
+function aptPlotDataHists(stats)
 %APTPLOTSUMMARYRESULTS Summary of this function goes here
 %   Detailed explanation goes here
-close all
-
-if isfield(stats,'Index1SE')
-    lassoPlot(stats.beta,stats,'PlotType','CV')
-else
-    lassoPlot(stats.beta,stats)
-end
-
-seqlogo(sequence)
 
 if stats.doLog10
     figure
     subplot(2,1,1)
-    histogram(Y)
+    histogram(stats.Y)
     title('Histogram of log10 of means of MaxIncrease')
     
     subplot(2,1,2)
-    histogram(1./weightsY)
+    histogram(1./stats.weightsY)
     title('Histogram of standard errors (log10) calculated from replicates')
 else
     figure
     subplot(2,1,1)
-    histogram(Y,0:50:1100)
+    histogram(stats.Y,0:50:1100)
     title('Histogram of means of MaxIncrease')
     
     subplot(2,1,2)
-    histogram(1./weightsY,0:20:300)
+    histogram(1./stats.weightsY,0:20:300)
     title('Histogram of standard errors calculated from replicates')
 end
 
