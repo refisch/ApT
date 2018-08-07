7bd4d0b08cc81040c1be68e3a82733ef8b4123a6
Lukas Refisch <lukas.refisch@posteo.de>
2018-08-07 18:17:33 +0200

    bug fix of MSE formula

--- aptLassoPlot.m
+++ aptLassoPlot.m
@@ -14,7 +14,7 @@ for iY = 1:length(apt.Y)
     hold on
     Xreg = [ones(size(apt.Y{iY})), apt.predX'];
     betareg = regress(apt.Y{iY}(~isnan(apt.Y{iY})), Xreg((~isnan(apt.Y{iY})),:));
-    MSEreg = 1 / sum(apt.Y{iY}(~isnan(apt.Y{iY}))) * sum((apt.Y{iY}(~isnan(apt.Y{iY}))-Xreg(~isnan(apt.Y{iY}),:)*betareg).^2);
+    MSEreg = 1 / sum(~isnan(apt.Y{iY})) * sum((apt.Y{iY}(~isnan(apt.Y{iY}))-Xreg(~isnan(apt.Y{iY}),:)*betareg).^2);
     plot(axh,apt.stats(iY).Lambda, ones(1,length(apt.stats(iY).Lambda))*MSEreg,'r--');
     title(['Cross-validated MSE of Lasso fit for observable ' apt.data(1).obsName{iY}])
     hold off
